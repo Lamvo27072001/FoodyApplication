fadf8477d1cd5fb1e9066e4e9632cc9abe5c595d
Lamvo27072001 <lampham27072001@gmail.com>
2022-04-13 13:44:43 +0700

    2.Finish SlashScreen and create Loginpage and Mainpage

--- app/src/main/java/View/LoginActivity.java
+++ app/src/main/java/View/LoginActivity.java
@@ -1,16 +1,52 @@
 package View;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.facebook.FacebookSdk;
+
 import hcmute.phamvohonglam19110154.foodyapplication.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+    // Declare Variables
+    EditText txtUserNam, txtPassword;
+    Button btnLogin;
+    String tk = "admin";
+    String mk = "admin";
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+
+        // Bindings
+        txtUserNam = findViewById(R.id.txtUserName);
+        txtPassword = findViewById(R.id.txtPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        //functions
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(txtUserNam.getText().toString().equals(tk) && txtPassword.getText().toString().equals(mk))
+                {
+                    startActivity(new Intent(LoginActivity.this, MainPageActivity.class));
+                }
+                else
+                {
+                    Toast.makeText(LoginActivity.this, "Your username or password is not correct!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/View/MainPageActivity.java
@@ -0,0 +1,16 @@
+package View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import hcmute.phamvohonglam19110154.foodyapplication.R;
+
+public class MainPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_mainpage);
+    }
+}
